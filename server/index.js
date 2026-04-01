a1f2e4c2582b394c12849688e36d42ab8f29379c
sidharthsv18 <sidharthsv18@gmail.com>
2026-03-31 20:20:26 -0500

    Add dotenv support for database configuration

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const { Pool } = require('pg');
+require('dotenv').config();
 
 const app = express();
 app.use(cors());
